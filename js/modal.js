09ad0d1c6d5a18b443fadee3da2dbdcdb705bd48
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-07-26 14:25:14 +0300

    почти финиш

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    noscroll: document.querySelector("[data-noscroll]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.noscroll.classList.toggle("is-noscroll");
   }
 })();
